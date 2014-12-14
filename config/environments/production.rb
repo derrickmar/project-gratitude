ef382ee35e2c6c8957bacaa2754803e4815aa807
Derrick Mar <derrickmar1215@berkeley.edu>
2014-12-13 20:37:25 -0800

    change hostname for s3 to us-west

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ config.i18n.fallbacks = true
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_host_name => "s3-us-west-1.amazonaws.com"
     }
   }
 end
