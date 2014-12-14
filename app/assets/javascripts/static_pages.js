27d3356f1f36bf94fecfd6bba07a8a2edc118fe8
Arik Gadye <arikdovgadye@gmail.com>
2014-12-13 16:26:22 -0800

    add a bunch of front-end stuff

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    modals.init();
     masonry.init();
         });
 
@@ -12,5 +13,31 @@ var masonry = {
 });
             var imgLoad = imagesLoaded(container, function() {
                 msnry.layout();
-});        }
+        });
+    }
+}
+
+var modals = {
+    init: function() {
+        this.bindListeners();
+        this.startModalShow();
+    },
+    bindListeners: function() {
+        $('.form-control').on('keydown', function() {
+            this.displayNumChars();
+        }.bind(this));
+    },
+    displayNumChars: function() {
+        var numChars = $('.form-control').val().length;
+        var max = 250;
+        var total = max - numChars;
+        $('.num-chars').text(total);
+    },
+    startModalShow: function() {
+        $('#home-modal').modal('show');
+        $('.make-note').on('click', function(e) {
+            $('#home-modal').modal('hide');
+            $('#createModal').modal('show');
+        })
+    }
     }
