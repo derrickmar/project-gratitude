f0f760bce1bb0ef94c90f37c3aa024258c92fc41
Derrick Mar <derrickmar1215@berkeley.edu>
2014-12-27 15:31:42 -0800

    add share buttons on show note functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,6 +80,7 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
 		devise_parameter_sanitizer.for(:account_update) << [:avatar, :name]
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :avatar, :email, :password, :password_confirmation, :current_password) }
 		# devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
 	end
 end
