0a045362b1da93ed6db8539ca0ae97611e74a75d
Derrick Mar <derrickmar1215@berkeley.edu>
2014-12-29 23:22:23 -0600

    still working on getting layoutComplete callback to cal randomRotate correctly with laidOutItems

--- app/assets/javascripts/page_specific.js
+++ app/assets/javascripts/page_specific.js
@@ -45,50 +45,54 @@ NotesController.prototype.all = function() {
                             topPosition: function(itemElem) {
                                 // we don't know what position the element will be at this time
                                 // so we can't sort by the positioning of the element
-                                // $(itemElem).
-                                // console.log("GET SORT DATA");
-                                // console.log(itemElem);
-                                // console.log("style", $(itemElem)[0].style);
-                                // console.log("style", $(itemElem)[0].style.cssText);
-                                // console.log("style", $(itemElem)[0].style.length);
-                                // console.log("style", $(itemElem)[0].style.top);
-                                // console.log("style2", $(itemElem).css("top"));
-                                // console.log($(itemElem)[0].style.top);
                                 var topPos = parseInt($(itemElem).css("top"));
-                                console.log(topPos);
+                                // console.log(topPos);
                                 return topPos;
                             }
                         },
                         sortBy: 'topPosition',
                     });
                     // isotopy.addTopPositions($('.note-holder-link'));
+                    // Need to call updateSortData after isotope assigns the top position style
                     isotopy.container.isotope('updateSortData').isotope();
-                    // isotopy.callbacks();
+                    isotopy.callbacks();
                 });
                 // triggering on layoutComplete here doesn't seem to work when outside of imagesLoaded
             },
 
             randomRotate: function(el) {
+                console.log("IN RANDOM ROTATE");
+                // debugger;
                 var randRotation = Math.floor((Math.random() * 5) + 1);
                 if (el.length == 1) {
                     randRotation *= Math.random() > 0.5 == 0 ? 1 : -1;
                     el.css("transform", "rotate(" + randRotation + "deg)")
                 } else {
                     el.each(function(index) {
+                        console.log("LOOPING");
                         // randRotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
                         randRotation *= index % 2 == 0 ? 1 : -1;
+                        console.log($(this));
                         $(this).css("transform", "rotate(" + randRotation + "deg)");
                     });
                 }
             },
             callbacks: function() {
                 console.log("CALLBACKS");
+                // laidOutItems is an Array of Isotope.Items
+                // TODO: Currently don't know how to convert laidOutItems to jquery 
                 isotopy.container.isotope('on', 'layoutComplete', function(isoInstance, laidOutItems) {
-                    console.log('trigger layoutComplete isotope');
-                    console.log(laidOutItems);
-                    isotopy.addTopPositions(laidOutItems);
+                    console.log('isotope: trigger layoutComplete event');
+                    console.log($(laidOutItems));
+                    isotopy.randomRotate($(laidOutItems));
+                    // debugger;
+                    // console.log(laidOutItems);
+                    // isotopy.addTopPositions(laidOutItems);
                 });
             },
+            // currently don't need to use the function because we are ordering
+            // by the position absolute automatically being inserted when isotope
+            // is called. 
             addTopPositions: function(el) {
                 console.log("lengthtoppos", el.length);
                 el.each(function() {
@@ -147,13 +151,13 @@ NotesController.prototype.all = function() {
                 // })
 
                 // Infinite scroll actions
-                $(".notes-holder").on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", ".note-holder-link", function(event) {
-                    // IF CASE IS TO PREVENT RANDOMROTATE BEING CALLED MULTIPLE TIMES FOR A NOTE HOLDER
-                    if (event.originalEvent.propertyName == 'opacity') {
-                        // console.log($(this));
-                        isotopy.randomRotate($(this));
-                    }
-                });
+                // $(".notes-holder").on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", ".note-holder-link", function(event) {
+                //     // IF CASE IS TO PREVENT RANDOMROTATE BEING CALLED MULTIPLE TIMES FOR A NOTE HOLDER
+                //     if (event.originalEvent.propertyName == 'opacity') {
+                //         // console.log($(this));
+                //         isotopy.randomRotate($(this));
+                //     }
+                // });
 
                 var search = location.search.substring(1);
                 var params = search ? JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
